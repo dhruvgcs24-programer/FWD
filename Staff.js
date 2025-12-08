1e888a9b44daf34d1533174fc2f6ddb0bbdcf567
dhruvgcs24-programer <dhruvg.cs24@bmsce.ac.in>
2025-12-09 02:06:06 +0530

    Delete Staff.js

--- Staff.js
+++
@@ -1,8 +0,0 @@
-// models/Staff.js
-const mongoose = require('mongoose');
-const staffSchema = new mongoose.Schema({
-    staffId: { type: String, required: true, unique: true }, 
-    password: { type: String, required: true }, 
-    role: { type: String, default: 'Nurse' } 
-});
-module.exports = mongoose.model('Staff', staffSchema);
